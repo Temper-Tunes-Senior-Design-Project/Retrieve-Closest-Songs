e89da8b99c19366ba840d36f3e12e03d33dadb54
usabah <uriyasabah309@gmail.com>
2023-04-16 15:59:06 -0400

    fixed issue with deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
-gcloud functions deploy moodswing-get-lyrics --gen2 --region=us-east1 --runtime=python310 --entry-point=closestSongs --trigger-http
+gcloud functions deploy moodswing-closest-songs --gen2 --region=us-east1 --runtime=python310 --entry-point=closestSongs --trigger-http
 
 Exit
